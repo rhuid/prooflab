7ddeb9b949d93b49fa7c597e3ee0982f8cdcb3fb
Ronald Huidrom <ronhuidrom@gmail.com>
2025-09-16 05:44:43 +0530

    mundane changes

--- preludes/scratch.lean
+++ preludes/scratch.lean
@@ -1,80 +1,52 @@
-/-
-  Modelling non-determinism using a custom Set monad.
 
-  Here, we build a monad Set from scratch, define set operations, prove that Set is a monad.
--/
 
--- Set as a function type
-def Set (α : Type) := α → Prop
 
--- Set membership
-def Set.mem (a : α) (s : Set α) : Prop := s a
-notation:50 a " ∈ " s => Set.mem a s
 
--- Empty set
-def Set.empty : Set α := fun _ => False
 
--- Set union and intersection
-def Set.union        (s t : Set α) : Set α := fun x => (x ∈ s) ∨ (x ∈ t)
-def Set.intersection (s t : Set α) : Set α := fun x => (x ∈ s) ∧ (x ∈ t)
-notation:50 s " ∪ " t => Set.union s t
-notation:50 s " ∩ " t => Set.intersection s t
+(p ∧ (q ∨ ¬r)) ∨ (¬p ∧ (r → q)) ∧ ¬(q ∧ r)
 
--- Set complement
-def Set.complement (s : Set α) : Set α := fun x => ¬ (x ∈ s)
+((p ∨ q) → r) ∧ ¬(p ∧ r) ∧ ¬(¬q ∨ p)
 
--- Examples
--- {2, 3, 5, 7}
-def primesLessThanTen : Set Nat := fun x => (x = 2) ∨ (x = 3) ∨ (x = 5) ∨ (x = 7)
+¬((p ∧ q) ∨ (¬p ∧ ¬q))
 
--- {1}
-def one : Set Nat := fun x => x = 1
 
--- {2, 3, 5, 7} intersection {1}
-def ourIntersection := primesLessThanTen ∩ one
 
--- Let's build the monad
--- Converts an element into a singleton set
-def Set.pure (a : α) : Set α := fun x => x = a
 
--- Combines all possibilities from s and applies f to each
-def Set.bind (s : Set α) (f : α → Set β) : Set β :=
-fun b => ∃ a, (a ∈ s) ∧ (b ∈ f a)
 
-instance Set.Monad : Monad Set where
-pure := Set.pure
-bind := Set.bind
 
--- Monad laws proofs
--- theorem pure_bind (a : α) (f : α → Set β) :
---     bind (pure a) f = f a := by
---   ext b
---   unfold bind Set.bind pure Set.pure Set.mem
---   constructor
---   · intro ⟨a', ⟨ha', hb⟩⟩
---     rw [ha'] at hb
---     exact hb
---   · intro h
---     exists a
---     constructor <;> assumption
 
--- theorem bind_pure (s : Set α) : bind s pure = s := by
---   ext x
---   unfold bind Set.bind pure Set.pure Set.mem
---   constructor
---   · intro ⟨a, ha, hx⟩
---     rw [hx] at ha
---     exact ha
---   · intro h
---     exists x
---     constructor <;> assumption
+p ∧ q ∧ r
 
--- theorem bind_assoc (s : Set α) (f : α → Set β) (g : β → Set γ) :
---     bind (bind s f) g = bind s (fun x => bind (f x) g) := by
---   ext c
---   unfold bind Set.bind Set.mem
---   constructor
---   · intro ⟨b, ⟨⟨a, ha, hb⟩, hc⟩⟩
---     exact ⟨a, ha, b, hb, hc⟩
---   · intro ⟨a, ha, b, hb, hc⟩
---     exact ⟨b, ⟨a, ha, hb⟩, hc⟩
+p ∨ q ∨ r
+
+(¬p ∨ ¬q) ∧ (¬q ∨ ¬r) ∧ (¬p ∨ ¬r)
+
+¬p ∧ ¬q ∧ ¬r
+
+(p ∧ ¬q ∧ ¬r) ∨ (¬p ∧ q ∧ ¬r) ∨ (¬p ∧ ¬q ∧ r)
+
+--
+
+For every person seated in some chair, we have the PBCs:
+
+x11 + x12 + x13 ≥ 1
+x21 + x22 + x23 ≥ 1
+x31 + x32 + x33 ≥ 1
+
+Its logical encoding is:
+
+(x11 ∨ x12 ∨ x13) ∧ (x21 ∨ x22 ∨ x23) ∧ (x31 ∨ x32 ∨ x33)
+
+--
+
+At most one person may be seated in a chair, we have the PBC:
+
+x11 + x21 + x31 ≤ 1
+x12 + x22 + x32 ≤ 1
+x13 + x23 + x33 ≤ 1
+
+Its logical encoding is:
+
+∧ ¬(x11 ∧ x21) ∧ ¬(x21 ∧ x31) ∧ ¬(x31 ∧ x11) ∧
+¬(x12 ∧ x22) ∧ ¬(x22 ∧ x32) ∧ ¬(x32 ∧ x12) ∧
+¬(x13 ∧ x23) ∧ ¬(x23 ∧ x33) ∧ ¬(x33 ∧ x13)
