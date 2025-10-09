a336a23848cff79e0a23d1c701680d00dff75ade
Ronald Huidrom <ronhuidrom@gmail.com>
2025-10-10 00:54:23 +0530

    new proposition

--- preludes/first-order-logic.lean
+++ preludes/first-order-logic.lean
@@ -2,6 +2,8 @@
 
 /- First order (predicate) logic -/
 
+set_option linter.unusedVariables false
+
 variable (α : Type)
 variable (p q : α → Prop)
 
@@ -17,3 +19,36 @@ example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
   fun hpq x => Or.elim hpq
   (fun hp => Or.inl (hp x))
   (fun hq => Or.inr (hq x))
+
+-- Bring a component of a formula outside a universal quantifier,
+-- when it does not depend on the quantified variable
+
+variable (r : Prop)
+
+example : α → ((∀ x : α, r) ↔ r) :=
+  fun a => Iff.intro
+  (fun h => h a)
+  (fun hr _ => hr)
+
+example : α → ((∀ x : α, r) ↔ r) := by
+  intro ha
+  apply Iff.intro
+  { intro h
+    exact h ha }
+  { intro hr
+    exact (fun _ => hr)}
+
+variable (r : Prop)
+
+example : α → ((∀ x : α, r) ↔ r) :=
+  fun a => Iff.intro
+  (fun h => h a)
+  (fun hr _ => hr)
+
+example : α → ((∀ x : α, r) ↔ r) := by
+  intro ha
+  apply Iff.intro
+  { intro h
+    exact h ha }
+  { intro hr
+    exact (fun _ => hr)}
