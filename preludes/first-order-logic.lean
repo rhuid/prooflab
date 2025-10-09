8d57943655e8fefb69bd95ed5fb54bf379687999
Ronald Huidrom <ronhuidrom@gmail.com>
2025-10-10 00:29:55 +0530

    more universal quantifiers

--- preludes/first-order-logic.lean
+++ preludes/first-order-logic.lean
@@ -5,6 +5,15 @@
 variable (α : Type)
 variable (p q : α → Prop)
 
-example : (∀ x, p x ∧ q x) → (∀ x, p x) ∧ (∀ x, q x) :=
-  fun h => And.intro
-  (fun w => (h w).1) (fun w => (h w).2)
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+  Iff.intro
+  (fun h => And.intro (fun w => (h w).1) (fun w => (h w).2))
+  (fun ⟨h₁, h₂⟩ x => And.intro (h₁ x) (h₂ x))
+
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) :=
+  fun hpq hp x => (hpq x) (hp x)
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+  fun hpq x => Or.elim hpq
+  (fun hp => Or.inl (hp x))
+  (fun hq => Or.inr (hq x))
