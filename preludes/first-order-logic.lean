5922889bc3d65113d77e81fbb7f3de25da3b0cf4
Ronald Huidrom <ronhuidrom@gmail.com>
2025-10-09 21:56:09 +0530

    universal quantifier

--- preludes/first-order-logic.lean
+++ preludes/first-order-logic.lean
@@ -1,3 +1,10 @@
 /- Exercises from the book: Theorem Proving in Lean 4 -/
 
 /- First order (predicate) logic -/
+
+variable (α : Type)
+variable (p q : α → Prop)
+
+example : (∀ x, p x ∧ q x) → (∀ x, p x) ∧ (∀ x, q x) :=
+  fun h => And.intro
+  (fun w => (h w).1) (fun w => (h w).2)
